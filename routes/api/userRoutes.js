5c2cd92f84db5dff61e913c4824c9be1473b8d8e
Moon Man <111019074+sergluna@users.noreply.github.com>
2023-02-28 20:01:38 -0600

    Additions to user routes and controllers

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,12 +3,19 @@ const {
     getUsers,
     getSingleUser,
     createUser,
+    updateUser,
+    deleteUser,
+    addFriend, 
+    removeFriend
 } = require('../../controllers/userController');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser);
+router.route('/:userId').get(getSingleUser).post(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/api/users/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;
