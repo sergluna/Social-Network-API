39b3bb7fa0e456e510c03249729f619e04561e6e
Moon Man <111019074+sergluna@users.noreply.github.com>
2023-02-28 23:27:45 -0600

    Correcting connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://localhost/', {
+connect('mongodb://localhost:27017', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   family: 4
